a3c3d58627525ab1a93865c2cbc5e22f079da373
Vida Maleki <vida.basmani@gmail.com>
2022-04-25 15:57:55 -0700

    wave one has been modified, planet file removed and planet class added to routes

--- app/__init__.py
+++ app/__init__.py
@@ -3,5 +3,13 @@ from flask import Flask
 
 def create_app(test_config=None):
     app = Flask(__name__)
-
+    from .routes import planets_bp
+    app.register_blueprint(planets_bp)
     return app
+
+class Book:
+    def __init__(self, id, title, description, author):
+        self.id = id
+        self.title = title
+        self.description = description
+        self.author = author
--- app/routes.py
+++ app/routes.py
@@ -1,2 +1,35 @@
-from flask import Blueprint
+from flask import Blueprint, jsonify
 
+class Planet():
+        def __init__(self, id, name, description,diameter_in_km):
+            self.id = id
+            self.name = name
+            self.description = description
+            self.diameter_in_km = diameter_in_km
+
+planets = [
+    Planet(11,"Mercury", "the fastest planet", 4879),
+    Planet(22,"Venus", "the brightest planet", 12104),
+    Planet(33,"Earth", "our own planet", 12756),
+    Planet(44,"Mars", "the red planet", 6792),
+    Planet(66,"Jupiter", "the largest planet", 142984),
+    Planet(77,"Saturn", "the slowest planet", 120536),
+    Planet(88,"Uranus", "named after the Greek god of the sky", 51118),
+    Planet(99,"Neptune", "a deep sea-blue colour", 49528)
+    ]
+
+planets_bp = Blueprint("planets", __name__, url_prefix="/planets")
+@planets_bp.route("", methods = ["GET"])
+
+def get_all_planets():
+    response = []
+    for planet in planets:
+        response.append(
+            {
+                "id": planet.id,
+                "name": planet.name,
+                "description": planet.description,
+                "diameter_in_km": planet.diameter_in_km
+            }
+        )
+    return jsonify(response)
