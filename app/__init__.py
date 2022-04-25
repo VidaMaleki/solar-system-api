c6c008876a023bd54ce2daeab49699094847b6f5
Vida Maleki <vida.basmani@gmail.com>
2022-04-25 16:14:01 -0700

    wave one is completed, init file is modified due to accidently added file

--- app/__init__.py
+++ app/__init__.py
@@ -7,9 +7,3 @@ def create_app(test_config=None):
     app.register_blueprint(planets_bp)
     return app
 
-class Book:
-    def __init__(self, id, title, description, author):
-        self.id = id
-        self.title = title
-        self.description = description
-        self.author = author
